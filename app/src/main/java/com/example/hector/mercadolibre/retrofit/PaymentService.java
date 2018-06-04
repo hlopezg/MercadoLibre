c644db7c23d4a5cd4af6969de6ddd5018d4360b2
Hector Lopez <sheva077@gmail.com>
2018-06-04 02:14:35 -0400

    First version

---
+++ app/src/main/java/com/example/hector/mercadolibre/retrofit/PaymentService.java
@@ -0,0 +1,24 @@
+package com.example.hector.mercadolibre.retrofit;
+
+import com.example.hector.mercadolibre.models.CardIssuers;
+import com.example.hector.mercadolibre.models.Installment;
+import com.example.hector.mercadolibre.models.PaymentMethod;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface PaymentService {
+    @GET("payment_methods")
+    Call<List<PaymentMethod>> getPaymentMethods(@Query("public_key") String publicKey);
+
+    @GET("payment_methods/card_issuers")
+    Call<List<CardIssuers>> getCardIssuers(@Query("public_key") String publicKey, @Query("payment_method_id") String paymentMethodId);
+
+    @GET("payment_methods/installments")
+    Call<List<Installment>> getInstallment(@Query("public_key") String publicKey, @Query("payment_method_id") String paymentMethodId, @Query("issuer.id") String issuerId);
+}
